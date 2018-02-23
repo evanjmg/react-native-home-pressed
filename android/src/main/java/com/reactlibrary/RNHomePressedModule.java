9f2efd841ed001f1dfcee94c0fa5bdc43f1fb9bb
evanjmg@gmail.com <evanjmg@gmail.com>
2018-02-23 18:10:38 +0000

    fix log

--- android/src/main/java/com/reactlibrary/RNHomePressedModule.java
+++ android/src/main/java/com/reactlibrary/RNHomePressedModule.java
@@ -5,6 +5,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import android.util.Log;
 
 public class RNHomePressedModule extends ReactContextBaseJavaModule {
 
