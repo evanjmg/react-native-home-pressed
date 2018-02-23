c2124d43a848e3845587878970b95b544639801a
evanjmg@gmail.com <evanjmg@gmail.com>
2018-02-23 18:17:33 +0000

    add module

--- android/src/main/java/com/reactlibrary/RNHomePressedModule.java
+++ android/src/main/java/com/reactlibrary/RNHomePressedModule.java
@@ -6,6 +6,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import android.util.Log;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 public class RNHomePressedModule extends ReactContextBaseJavaModule {
 
